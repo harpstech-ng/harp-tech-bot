6e6359b7508b6654da3c2f63653a0623594f299a
Okugbe Praise <okugbepraise@gmail.com>
2026-04-26 20:26:08 +0100

    Initialize services module with default exports

---
+++ services.js
@@ -0,0 +1,9 @@
+module.exports = {
+  SMM_SERVICES: [],
+  WEB_TIERS: {},
+  AIRTIME_NETWORKS: [],
+  DATA_PLANS: {},
+  BIZ_SERVICES: {},
+  findSmmService: () => null,
+  calcSmmCost: () => 0,
+};
